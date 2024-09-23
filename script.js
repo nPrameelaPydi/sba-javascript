e2ff9c403b3203460395a44c92f84bd2c4b37e6f
Prameela Pydi <nprameela.pydi@gmail.com>
2024-09-22 17:18:59 -0700

    modified script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-//// The provided course information.
-//const CourseInfo = {
-//    id: 451,
-//    name: "Introduction to JavaScript"
-//};
+// The provided course information.
+const CourseInfo = {
+    id: 451,
+    name: "Introduction to JavaScript"
+};
 
-//// The provided assignment group.
-//const AssignmentGroup = {
-//    id: 12345,
-//    name: "Fundamentals of JavaScript",
-//    course_id: 451,
-//    group_weight: 25,
-//    assignments: [
-//        {
-//            id: 1,
-//            name: "Declare a Variable",
-//            due_at: "2023-01-25",
-//            points_possible: 50
-//        },
-//        {
-//            id: 2,
-//            name: "Write a Function",
-//            due_at: "2023-02-27",
-//            points_possible: 150
-//        },
-//        {
-//            id: 3,
-//            name: "Code the World",
-//            due_at: "3156-11-15",
-//            points_possible: 500
-//        }
-//    ]
-//};
+// The provided assignment group.
+const AssignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: [
+        {
+            id: 1,
+            name: "Declare a Variable",
+            due_at: "2023-01-25",
+            points_possible: 50
+        },
+        {
+            id: 2,
+            name: "Write a Function",
+            due_at: "2023-02-27",
+            points_possible: 150
+        },
+        {
+            id: 3,
+            name: "Code the World",
+            due_at: "3156-11-15",
+            points_possible: 500
+        }
+    ]
+};
 
-//// The provided learner submission data.
+// The provided learner submission data.
 const LearnerSubmissions = [
     {
         learner_id: 125,
@@ -76,58 +76,70 @@ const LearnerSubmissions = [
     }
 ];
 
-//function getAssignInfo(assign_id) {
-//    for (let i = 0; AssignmentGroup.assignments.length; i++) {
+function getAssignInfo(assign_id) {
+    for (let i = 0; AssignmentGroup.assignments.length; i++) {
 
-//        if (AssignmentGroup.assignments[i].id == assign_id) {
-//            return (AssignmentGroup.assignments[i]);
-//        };
+        if (AssignmentGroup.assignments[i].id == assign_id) {
+            return (AssignmentGroup.assignments[i]);
+        };
 
-//    }
-//}
-////console.log(getAssignInfo(2));
+    }
+}
+//console.log(getAssignInfo(2));
+let score = 0;
+let results = [];
 
-//for (let i = 0; i < LearnerSubmissions; i++) {
-//    //let learnerSubInfo = LearnerSubmissions[i];
-//    console.log(getAssignInfo(LearnerSubmissions[i].assignment_id));
-//    //console.log(assignInfo);
-//}
+for (let i = 0; i < LearnerSubmissions.length; i++) {
+    //console.log(LearnerSubmissions[i].assignment_id);
+    //console.log((getAssignInfo(LearnerSubmissions[i].assignment_id)));
+    let assignInfo = getAssignInfo(LearnerSubmissions[i].assignment_id);
 
+    if (LearnerSubmissions[i].submission.submitted_at > assignInfo.due_at) {
+        score = LearnerSubmissions[i].submission.score * 0.9;
+    }
 
-//function getLearnerData(course, ag, submissions) {
-//    //Code goes here
-
-
-
-
-
+    let finalScorePerAssign = score / assignInfo.points_possible;
 
 
 
+}
+
+
+
+function getLearnerData(course, ag, submissions) {
+    //Code goes here
 
 
 
 
 
-//    // here, we would process this data to achieve the desired result.
-//    const result = [
-//        {
-//            id: 125,
-//            avg: 0.985, // (47 + 150) / (50 + 150)
-//            1: 0.94, // 47 / 50
-//            2: 1.0 // 150 / 150
-//        },
-//        {
-//            id: 132,
-//            avg: 0.82, // (39 + 125) / (50 + 150)
-//            1: 0.78, // 39 / 50
-//            2: 0.833 // late: (140 - 15) / 150
-//        }
-//    ];
 
-//    return result;
-//}
+
+
+
+
+
+
+
+    // here, we would process this data to achieve the desired result.
+    //const result = [
+    //    {
+    //        id: 125,
+    //        avg: 0.985, // (47 + 150) / (50 + 150)
+    //        1: 0.94, // 47 / 50
+    //        2: 1.0 // 150 / 150
+    //    },
+    //    {
+    //        id: 132,
+    //        avg: 0.82, // (39 + 125) / (50 + 150)
+    //        1: 0.78, // 39 / 50
+    //        2: 0.833 // late: (140 - 15) / 150
+    //    }
+    //];
+
+    //return result;
+}
 
 //const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
-////console.log(result);
+//console.log(result);
--- script2.js
+++ script2.js
@@ -1,44 +1,46 @@
-const AssignmentGroup = {
-    id: 12345,
-    name: "Fundamentals of JavaScript",
-    course_id: 451,
-    group_weight: 25,
-    assignments: [
-        {
-            id: 1,
-            name: "Declare a Variable",
-            due_at: "2023-01-25",
-            points_possible: 50
-        },
-        {
-            id: 2,
-            name: "Write a Function",
-            due_at: "2023-02-27",
-            points_possible: 150
-        },
-        {
-            id: 3,
-            name: "Code the World",
-            due_at: "3156-11-15",
-            points_possible: 500
-        }
-    ]
-};
+//const AssignmentGroup = {
+//    id: 12345,
+//    name: "Fundamentals of JavaScript",
+//    course_id: 451,
+//    group_weight: 25,
+//    assignments: [
+//        {
+//            id: 1,
+//            name: "Declare a Variable",
+//            due_at: "2023-01-25",
+//            points_possible: 50
+//        },
+//        {
+//            id: 2,
+//            name: "Write a Function",
+//            due_at: "2023-02-27",
+//            points_possible: 150
+//        },
+//        {
+//            id: 3,
+//            name: "Code the World",
+//            due_at: "3156-11-15",
+//            points_possible: 500
+//        }
+//    ]
+//};
 
 
-function getAssignInfo(assign_id) {
-    for (let i = 0; AssignmentGroup.assignments.length; i++) {
+//function getAssignInfo(assign_id) {
+//    for (let i = 0; AssignmentGroup.assignments.length; i++) {
 
-        if (AssignmentGroup.assignments[i].id == assign_id) {
-            return (AssignmentGroup.assignments[i]);
-        };
+//        if (AssignmentGroup.assignments[i].id == assign_id) {
+//            return (AssignmentGroup.assignments[i]);
+//        };
 
-    }
-}
-//console.log(getAssignInfo(2));
+//    }
+//}
+////console.log(getAssignInfo(2));
 
-for (let i = 0; i < LearnerSubmissions.length; i++) {
-    console.log(LearnerSubmissions[i].assignment_id);
-}
+//for (let i = 0; i < LearnerSubmissions.length; i++) {
+//    console.log(LearnerSubmissions[i].assignment_id);
+//    console.log(getAssignInfo(LearnerSubmissions[i].assignment_id).due_at);
 
-console.log(LearnerSubmissions.length);
+//}
+
+////console.log(LearnerSubmissions.length);
